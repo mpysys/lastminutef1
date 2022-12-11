c258a24160cec5972f8bb5320a05bb449a2d309d
mpysys <monnet.morgan@gmail.com>
2022-12-11 20:33:17 +0000

    added product views and templates

--- lastminutef1/tickets/admin.py
+++ lastminutef1/tickets/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Ticket, Category
 # Register your models here.
 
-admin.site.register(Ticket)
-admin.site.register(Category)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'race',
+        'country',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Category, CategoryAdmin)
--- lastminutef1/tickets/views.py
+++ lastminutef1/tickets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Ticket
 
 # Create your views here.
+
+def all_tickets(request):
+    """ A view to show all tickets, including sorting and search queries """
+
+    tickets = Ticket.objects.all()
+
+    context = {
+        'tickets': tickets,
+    }
+
+    return render(request, 'tickets.html', context)
