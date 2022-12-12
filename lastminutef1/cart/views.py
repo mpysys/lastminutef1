c08823e3cf0a6ded510baab001218a290de94093
mpysys <monnet.morgan@gmail.com>
2022-12-12 16:56:53 +0000

    added add_to_cart functionality

--- lastminutef1/cart/views.py
+++ lastminutef1/cart/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified tickets to the shopping Cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    print(request.session['cart'])
+    return redirect(redirect_url)
