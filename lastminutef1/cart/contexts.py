b991b9260c0fb6f087220a96485c125ed9bd821f
mpysys <monnet.morgan@gmail.com>
2022-12-12 17:50:00 +0000

    updated context processor with discount and css

--- lastminutef1/cart/contexts.py
+++ lastminutef1/cart/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from tickets.models import Ticket
+
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     ticket_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        ticket = get_object_or_404(Ticket, pk=item_id)
+        total += quantity * ticket.price
+        ticket_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'ticket': ticket,
+        })
 
     if total < settings.DISCOUNT_THRESHOLD:
         discount = total * Decimal(settings.STANDARD_PROMOTION_PERCENTAGE  / 100)
