21f2161d5273b9e6398e45328bbe1e307b241fe7
mpysys <monnet.morgan@gmail.com>
2022-12-13 15:55:05 +0000

    added methods, forms, admin and signals

--- lastminutef1/checkout/admin.py
+++ lastminutef1/checkout/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import TicketOrder, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class TicketOrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('ticket_number', 'date',
+                       'discount_value', 'order_total',
+                       'grand_total',)
+
+    fields = ('ticket_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'discount_value',
+              'order_total', 'grand_total',)
+
+    list_display = ('ticket_number', 'date', 'full_name',
+                    'order_total', 'discount_value',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(TicketOrder, TicketOrderAdmin)
