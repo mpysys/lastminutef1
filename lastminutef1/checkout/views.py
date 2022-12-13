7d8c915a7a5d6d46eccadc354f9a54b61f7ab34e
mpysys <monnet.morgan@gmail.com>
2022-12-13 17:24:21 +0000

    added stripe_elements.js and started to plug in

--- lastminutef1/checkout/views.py
+++ lastminutef1/checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout.html'
     context = {
         'ticket_order_form': ticket_order_form,
+        'stripe_public_key': 'pk_test_51MBnrnDhtt0ii4VPb4IM65cqw91CBqmzS4ZiVYxjQ7RK5hWysKflAV42kCz9OITXjK9RK9OHny8pay1RsDIrY5x50031bPwNY1',
+        'client_secret' : 'test client secret',
     }
 
     return render(request, template, context)
