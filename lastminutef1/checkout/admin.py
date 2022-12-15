4bb99608599abd4e5c1d4b655c529ae88f70d992
mpysys <monnet.morgan@gmail.com>
2022-12-15 08:50:13 +0000

    added order history and updated the checkout.views

--- lastminutef1/checkout/admin.py
+++ lastminutef1/checkout/admin.py
@@ -14,7 +14,7 @@ class TicketOrderAdmin(admin.ModelAdmin):
                        'discount_value', 'order_total',
                        'grand_total', 'original_cart', 'stripe_pid')
 
-    fields = ('ticket_number', 'date', 'full_name',
+    fields = ('ticket_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'discount_value',
--- lastminutef1/checkout/webhook_handler.py
+++ lastminutef1/checkout/webhook_handler.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from .models import TicketOrder, OrderLineItem
 from tickets.models import Ticket
+from userprofiles.models import UserProfile
+
 import json
 import time
 import stripe
@@ -42,6 +44,21 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+            
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -75,6 +92,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=user_profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
