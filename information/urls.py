0b258b80cef33bc8e1247b4dc3ebfe06eb479493
mpysys <monnet.morgan@gmail.com>
2022-12-16 13:51:18 +0000

    add paddock html

--- information/urls.py
+++ information/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.schedule, name='information')
+    path('paddock', views.paddock, name='paddock')
 ]
--- information/views.py
+++ information/views.py
@@ -7,3 +7,9 @@ def schedule(request):
     """ A view to return the index page """
 
     return render(request, 'schedule.html')
+
+
+def paddock(request):
+    """ A view to return the index page """
+
+    return render(request, 'paddock.html')
