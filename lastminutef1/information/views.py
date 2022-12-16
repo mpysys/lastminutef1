4e4a50fcf9358d906be5c57e6560fba72d9de7bc
mpysys <monnet.morgan@gmail.com>
2022-12-16 00:39:52 +0000

    added schedule page, footer and relative css styling

---
+++ lastminutef1/information/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def schedule(request):
+    """ A view to return the index page """
+
+    return render(request, 'schedule.html')
--- lastminutef1/lastminutef1/urls.py
+++ lastminutef1/lastminutef1/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('userprofiles.urls')),
+    path('information/', include('information.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'lastminutef1.views.handler404'
